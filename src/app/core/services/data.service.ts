03ce47322d87146aaa686a3642bed74abac576a2
nicolasnevraumont <nnevraumont@gmail.com>
2020-02-06 12:27:40 +0100

    Adding data service for getting products from API and display them in home page

--- src/app/core/services/data.service.ts
+++ src/app/core/services/data.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
 import { environment } from '../../../environments/environment';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
 import { Product } from '../models/product';
+import { catchError, retry } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -15,6 +16,22 @@ export class DataService {
   }
 
   public sendGetRequest(): Observable<Product[]> {
-    return this.httpClient.get<Product[]>(this.REST_API_SERVER);
+    // on fail, retries 3 times the httpcall before producing an error
+    // TODO: might add some time delay to the retry
+    return this.httpClient.get<Product[]>(this.REST_API_SERVER).pipe(retry(3), catchError(this.handleError));
+  }
+
+  // TODO: move to HttpClient interceptors
+  handleError(error: HttpErrorResponse) {
+    let errorMessage;
+    if (error.error instanceof ErrorEvent) {
+      // Client-side errors
+      errorMessage = `Client-side error: ${error.error.message}`;
+    } else {
+      // Server-side errors
+      errorMessage = `Server-side error: Code: ${error.status}\nMessage: ${error.message}`;
+    }
+    window.alert(errorMessage);
+    return throwError(errorMessage);
   }
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,22 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { DataService } from '../core/services/data.service';
 import { Product } from '../core/models/product';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
 
   products: Product[] = [];
+  destroy$: Subject<boolean> = new Subject<boolean>(); // used to unsubscribe from http calls if the component is destroyed (closed)
 
   constructor(private dataService: DataService) { }
 
   ngOnInit() {
-    this.dataService.sendGetRequest().subscribe((data: Product[]) => {
-      console.log(data);
+    this.dataService.sendGetRequest().pipe(takeUntil(this.destroy$)).subscribe((data: Product[]) => {
       this.products = data;
     });
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next(true);
+    // Unsubscribe from the subject
+    this.destroy$.unsubscribe();
   }
 }
